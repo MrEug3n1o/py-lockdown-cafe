f49d8c3120fdeab4221f46b031a694cb70d59d7f
mr_eugene <roberttimethone@gmail.com>
2025-07-28 11:00:15 +0200

    sol1

--- app/main.py
+++ app/main.py
@@ -3,19 +3,14 @@ from app.cafe import Cafe
 
 
 def go_to_cafe(friends: list, cafe: Cafe) -> str:
-    can_go_to_cafe = True
     masks_to_buy = 0
-
     for friend in friends:
         try:
             cafe.visit_cafe(friend)
         except errors.VaccineError:
-            can_go_to_cafe = False
             return "All friends should be vaccinated"
         except errors.NotWearingMaskError:
-            can_go_to_cafe = False
             masks_to_buy += 1
-
-    if can_go_to_cafe:
-        return f"Friends can go to {cafe.name}"
-    return f"Friends should buy {masks_to_buy} masks"
+    if masks_to_buy:
+        return f"Friends should buy {masks_to_buy} masks"
+    return f"Friends can go to {cafe.name}"
